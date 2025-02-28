fbbcc5677ed6a8fce6dc4523dc868fca1734fb77
sramisetty1 <sramisetty1@miraclesoft.com>
2025-02-28 20:14:19 +0530

    changed vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_BACKEND_URL || "http://localhost:5050", // Uses env variable if available
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Remove "/api" from request
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
